90022d5b5b245e46674e786e4841bc0d8f1a1191
Tyler D <tyler@tdaigle.com>
2021-04-26 15:21:22 -0400

    Trying to clean up some routes - added sections and sublement routes

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const Question = require("./models/Question");
 const SubElement = require("./models/SubElement");
+const Section = require("./models/Section");
+
 const { loadExamData } = require("./utils/examData");
 const port = 8080;
+
+// Connect to the server
 mongoose
   .connect("mongodb://localhost:27017/ham_test", {
     useNewUrlParser: true,
@@ -12,6 +16,9 @@ mongoose
   })
   .then(async () => {
     console.log("Connected to database. Starting Server");
+
+    // examData is just an object that makes it easier to get info about the exams.
+    // examData = { T: {tech exam data}, G: {general exam data}, E: {amateur extra exam data}}
     const examData = await loadExamData();
     startServer(examData);
   })
@@ -25,6 +32,7 @@ process.on("exit", () => {
   mongoose.connection.close();
 });
 
+// called after the server has successfully started
 function startServer(examData) {
   const app = express();
   app.use(cors());
@@ -34,67 +42,54 @@ function startServer(examData) {
     res.send("Hello, World!");
   });
 
-  // TODO: Get all questions in each subelement?
+  app.get("/question/:questionId", function (req, res) {
+    Question.findOne({ id: req.params.questionId }).then(data => res.json(data));    
+  });
 
-  app.get("/questions", function (req, res) {
-    // get questions by ID, example: /questions?id=T1A01
-    if ("id" in req.query) {
-      Question.findOne({ id: req.query.id }, (err, data) => {
-        if (!err) {
-          if (data === null) {
-            res.json({ message: "Question Not Found" });
-          } else {
-            res.json(data);
-          }
-        }
-      });
-    } else if ("section" in req.query) {
-      // get all questions in a section
-      // Example: /questions?section=T1A
-
-      const { section } = req.query;
-
-      Question.find({ section_id: section }).then(data => res.json({ count: data.length, data })).catch(err => res.json({ msg: err }));
-    } else {
-      res.send("Error");
-    }
+  // /questions/section/sectionId will get all the questions that are in the section
+  app.get("/questions/section/:sectionId", function (req, res) {
+    Question.find({ section_id: req.params.sectionId }).then(data => res.json(data));
   });
 
   // /exams routes return data about each exam, such as number of questions and the sections that
-  // each exam covers.
+  // each exam covers. Right now it just uses the examData object rather than actually querying.
   app.get("/exams", function (req, res) {
     res.json(examData);
   });
+
   app.get("/exams/technician", function (req, res) {
     res.json(examData.T)
-  });
-
-  app.get("/exams/general", function (req, res) {
-    res.json(examData.G)
-  });
-
-  app.get("/exams/extra", function (req, res) {
-    res.json(examData.E)
-  });
-
-  app.get("/subelements", function (req, res) {
-    SubElement.find().then(data => res.json(data));
-  });
-
-  app.get("/subelement/:subId", function(req, res) {
     console.log(req.params.subId);
-    SubElement.findOne({subelement_id : req.params.subId}).then(data => res.json(data));    
-  });
-  app.get("/subelements/general", function (req, res) {
-    SubElement.find({ subelement_id: { $regex: /G[0-9]/ } }).then(data => res.json(data));
   });
 
-  app.get("/subelements/technician", function (req, res) {
-    SubElement.find({ subelement_id: { $regex: /T[0-9]/ } }).then(data => res.json(data));
+  // /subelement/:subId gets the description of a specific subelement (subId)
+  app.get("/subelement/:subId", function (req, res) {
+    SubElement.findOne({ subelement_id: req.params.subId }).then(data => res.json(data));
   });
 
-  app.get("/subelements/extra", function (req, res) {
-    SubElement.find({ subelement_id: { $regex: /E[0-9]/ } }).then(data => res.json(data));
+  // app.get("/subelements/general", function (req, res) {
+  //   SubElement.find({ subelement_id: { $regex: /G[0-9]/ } }).then(data => res.json(data));
+  // });
+
+  // app.get("/subelements/technician", function (req, res) {
+  //   SubElement.find({ subelement_id: { $regex: /T[0-9]/ } }).then(data => res.json(data));
+  // });
+
+  // app.get("/subelements/extra", function (req, res) {
+  //   SubElement.find({ subelement_id: { $regex: /E[0-9]/ } }).then(data => res.json(data));
+  // });
+
+  // get ALL the sections and their descriptions
+  app.get("/sections", function (req, res) {
+    Section.find().then(data => res.json(data));
+  });
+
+  // get all the sections in a subelement
+  // Example: http://localhost/sections/subelement/T1 will get all of the details for all
+  // of the sections that are in the subelement T1
+
+  app.get("/sections/subelement/:subId", function (req, res) {
+    Section.find({ subelement_id: req.params.subId }).then(data => res.json(data));
   });
 
   app.listen(port, () => {
